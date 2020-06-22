997ffc4d0b78b2a0801be694520b7c0fac31f474
Matt-st <matthew.thorne.s@gmail.com>
2020-06-22 15:08:53 -0400

    Added sys error for integration test failure

--- integration-test/numberConversionApiTest.py
+++ integration-test/numberConversionApiTest.py
@@ -1,55 +1,57 @@
 import requests
+import sys
 from asserts import assert_true, assert_equal, assert_raises
 
 def main():
     print("Executing request...")
     print("Converting numbers..")
+    try:
+        payload = {'number': '123412341224'}
+        r = requests.get('http://localhost:8080/num_to_english', params=payload)
 
-    payload = {'number': '123412341224'}
-    r = requests.get('http://localhost:8080/num_to_english', params=payload)
+        print("Converted 123412341224 to", r.json()["num_in_english"])
+        assert_equal("one hundred twenty three billion four hundred twelve million three hundred forty one thousand two hundred twenty four", r.json()["num_in_english"])
+        assert_equal(200, r.status_code)
 
-    print("Converted 123412341224 to", r.json()["num_in_english"])
-    assert_equal("one hundred twenty three billion four hundred twelve million three hundred forty one thousand two hundred twenty four", r.json()["num_in_english"])
-    assert_equal(200, r.status_code)
+        payload = {'number': '1'}
+        r = requests.get('http://localhost:8080/num_to_english', params=payload)
+        print("Converted 1 to", r.json()["num_in_english"])
+        assert_equal("one", r.json()["num_in_english"])
+        assert_equal(200, r.status_code)
 
-    payload = {'number': '1'}
-    r = requests.get('http://localhost:8080/num_to_english', params=payload)
-    print("Converted 1 to", r.json()["num_in_english"])
-    assert_equal("one", r.json()["num_in_english"])
-    assert_equal(200, r.status_code)
+        payload = {'number': '19'}
+        r = requests.get('http://localhost:8080/num_to_english', params=payload)
+        print("Converted 19 to", r.json()["num_in_english"])
+        assert_equal("nineteen", r.json()["num_in_english"])
+        assert_equal(200, r.status_code)
 
-    payload = {'number': '19'}
-    r = requests.get('http://localhost:8080/num_to_english', params=payload)
-    print("Converted 19 to", r.json()["num_in_english"])
-    assert_equal("nineteen", r.json()["num_in_english"])
-    assert_equal(200, r.status_code)
+        payload = {'number': '28000'}
+        r = requests.get('http://localhost:8080/num_to_english', params=payload)
+        print("Converted 28000 to", r.json()["num_in_english"])
+        assert_equal("twenty eight thousand", r.json()["num_in_english"])
+        assert_equal(200, r.status_code)
 
-    payload = {'number': '28000'}
-    r = requests.get('http://localhost:8080/num_to_english', params=payload)
-    print("Converted 28000 to", r.json()["num_in_english"])
-    assert_equal("twenty eight thousand", r.json()["num_in_english"])
-    assert_equal(200, r.status_code)
+        payload = {'number': '$28000'}
+        r = requests.get('http://localhost:8080/num_to_english', params=payload)
+        print("Attempting to convert $28000  and expecting a currency error", r.json()["status"])
+        assert_equal(422, r.status_code)
 
-    payload = {'number': '$28000'}
-    r = requests.get('http://localhost:8080/num_to_english', params=payload)
-    print("Attempting to convert $28000  and expecting a currency error", r.json()["status"])
-    assert_equal(422, r.status_code)
+        payload = {'number': '.28000'}
+        r = requests.get('http://localhost:8080/num_to_english', params=payload)
+        print("Attempting to convert .28000  and expecting a currency error", r.json()["status"])
+        assert_equal(422, r.status_code)
 
-    payload = {'number': '.28000'}
-    r = requests.get('http://localhost:8080/num_to_english', params=payload)
-    print("Attempting to convert .28000  and expecting a currency error", r.json()["status"])
-    assert_equal(422, r.status_code)
+        payload = {'number': '/28000'}
+        r = requests.get('http://localhost:8080/num_to_english', params=payload)
+        print("Attempting to convert /28000  and expecting a currency error", r.json()["status"])
+        assert_equal(422, r.status_code)
 
-    payload = {'number': '/28000'}
-    r = requests.get('http://localhost:8080/num_to_english', params=payload)
-    print("Attempting to convert /28000  and expecting a currency error", r.json()["status"])
-    assert_equal(422, r.status_code)
+        print("Integration successful.")
+        sys.exit(0)
 
+    except:
+        sys.exit(1)
 
-
-    print("Integration successful.")
-
-    sys.exit(100)
 
 
 if __name__ == "__main__":
